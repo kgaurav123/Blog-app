21f2d82007981ba0d009ae9ef99b7ee7a2a8eac4
Kumar Gaurav <48727152+kgaurav123@users.noreply.github.com>
2020-05-05 22:46:19 +0530

    blog frontend

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'blog/index.html')
+def blogpost(request):
+    return render(request,'blog/blogpost.html')
--- MyBlog/home/views.py
+++ MyBlog/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
 from django.shortcuts import render,HttpResponse
 from.models import Contact
@@ -12,8 +12,12 @@ def contact(request):
         email = request.POST['email']
         phone = request.POST['phone']
         content = request.POST['content']
-        contact = Contact(name=name,email=email,phone=phone,content=content)
-        contact.save()
+        if len(name)<3 or len(email)<6 or len(phone)<10 or len(content)<10:
+            messages.error(request,'Please fill the form correctly')
+        else:
+            contact = Contact(name=name,email=email,phone=phone,content=content)
+            contact.save()
+            messages.success(request,'Your messages have been sent successfully')
     return render(request,'home/contact.html')
 
 def about(request):
