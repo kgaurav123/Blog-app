888ba08c292138cfc3dc288085f76b537da1aba5
Kumar Gaurav <48727152+kgaurav123@users.noreply.github.com>
2020-05-05 13:02:17 +0530

    deletion

--- MyBlog/manage.py
+++ MyBlog/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Myblog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBlog.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
